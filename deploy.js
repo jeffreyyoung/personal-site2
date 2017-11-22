e8fe17aa6d7a61451b951712204e8f3373a6ce13
Jeffrey Young <jeyoung@adobe.com>
2017-11-21 21:52:18 -0700

    meow

--- deploy.js
+++ deploy.js
@@ -1,8 +1,8 @@
 var ghpages = require('gh-pages');
-
+console.log('DEPLOYING');
 ghpages.publish('build', {
   branch: 'master',
   repo: 'https://github.com/jeffreyyoung/jeffreyyoung.github.io.git'
 }, function(e) {
-	console.log('HEREEE', e);
+	console.log('ERROR', e);
 });
