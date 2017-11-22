7e22f89d272f262e3b7a76fa448fb4759d8a50d9
Jeffrey Young <jeyoung@adobe.com>
2017-11-22 01:38:48 -0700

    meeeoOW

--- src/components/BackArrow.js
+++ src/components/BackArrow.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
-import backarrow from './thin-arrow-pointing-left.svg';
 import './BackArrow.css'
 export default ({to,children, className}) => (
 	<nav className={className}>
 		<Link className='BackArrow no-underline underline-hover blue' to={to}>
-			<img className='backarrowicon' alt='' src={backarrow}/>{children}
+			<span role='img' aria-label='back'>👈</span>{children}
 		</Link>
 	</nav>
 )
